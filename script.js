be10b8ac982e832e1903c12a7651760f7c5bf5e2
R Anggraini <northwoodjp@gmail.com>
2026-03-03 13:08:28 +0900

    update fundraising deadline and progress

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ faders.forEach((el) => observer.observe(el));
 
 
 /* ---------- COUNTDOWN TIMER ---------- */
-const targetDate = new Date("February 28, 2026 23:59:59").getTime();
+const targetDate = new Date("April 1, 2026 23:59:59").getTime();
 
 function updateCountdown() {
   const now = new Date().getTime();
